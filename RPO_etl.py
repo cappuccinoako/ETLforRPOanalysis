e1d152963dfd6699454e8d4f82b8d809a2bd908e
cappuccinoako <chacuttbudee.k@gmail.com>
2025-03-26 16:54:48 +0700

    dfSample function

--- RPO_etl.py
+++ RPO_etl.py
@@ -12,23 +12,41 @@ import numpy as np
 #     values = [wb.active[cell].value for cell in cells]
 #     return(values)
 
-def readWB(path):
-    """read workbook"""
+def loadWB(path):
+    """load and cleann data from excel file"""
     combined_df = pd.DataFrame()
     dfs = pd.read_excel(path, sheet_name=None)
     for key, value in dfs.items():
+        #skipped 6 rows
         skipped_value = value.iloc[7:58]
         skipped_value = skipped_value.transpose()
+        #get header
         new_header = skipped_value.iloc[0]
         skipped_value = skipped_value[1:]
         skipped_value.columns = new_header
+        #remove first 2 empty row
         skipped_value = skipped_value.iloc[2:]
+        #combine
         combined_df = pd.concat([combined_df, skipped_value])
-        combined_df = combined_df.dropna(how='all')
+    combined_df = combined_df.dropna(how='all')
+    #drop that annoying index column
+    combined_df = combined_df.reset_index(drop=True)
+    combined_df = combined_df.rename_axis(None, axis=1)
+    combined_df = combined_df.rename(columns={'Critical Parameter Numbers à':'Critical Parameter Numbers'})
     return combined_df
 
-# readWB(path='Data\Machanical (Suspension).xlsx')
+def getSample(loaded_df):
+    dfSample = loaded_df.iloc[:, [0] + list(range(19, 51))]
+    melted_df = dfSample.melt(
+        id_vars=['Critical Parameter Numbers'],  # Column to keep as identifier
+        value_vars=[f'Sample #{i}' for i in range(1, 33)],  # Columns to collapse
+        value_name='Sample Value'  # Name of the column containing the values
+    )
+    melted_df_cleaned = melted_df.dropna(subset=['Sample Value'])
+    return melted_df_cleaned
 
+# loaded_df = loadWB(path='Data\Machanical (Suspension).xlsx')
+# print(getSample(loaded_df))
 
 
 def dataQuality(loaded_df):
